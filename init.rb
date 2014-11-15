a031a27ee26a62e2238f4aed1de4bb14774e3f6f
Felix Schäfer <felix@thegcat.net>
2014-11-15 21:59:02 +0100

    Update for Redmine 2.5

--- init.rb
+++ init.rb
@@ -1,16 +1,15 @@
 require 'redmine'
-
 require 'auto_watchers/hooks/auto_watchers_issue_hook'
 
 Redmine::Plugin.register :redmine_auto_watchers do
   name 'Automatic Watchers'
-  author 'Robert Chady'
+  author 'Robert Chady, Felix Schäfer'
   description 'Automatic Watchers is a Redmine project that automatically adds people that touch an issue as watchers."'
-  version '0.3.0'
+  version '1.0.0'
 
-  requires_redmine :version_or_higher => '0.9.0'
+  requires_redmine '2.5'
 
-  settings :default => {
-               'enable_watchers' => 'on'
-            }, :partial => 'settings/auto_watchers'
+  settings default:
+    {'enable_watchers' => 'on'},
+    partial: 'settings/auto_watchers'
 end
--- lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
+++ lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
@@ -1,20 +1,23 @@
 # Hook in to the Issues controller and update the watchers before it is
 # saved.
 class AutoWatchersIssuesControllerHook < Redmine::Hook::ViewListener
-
   def controller_issues_edit_before_save(context={})
     configuration = Setting.plugin_redmine_auto_watchers
     if configuration['enable_watchers']
       issue = context[:issue]
+      # Add the current user if they are addable
+      issue.add_watcher(User.current) unless issue.watched_by?(User.current)
+
       # Add the person the ticket is assigned to if it is assigned to
       # someone and they are addable.
-      if !issue.assigned_to.blank? and 
-         issue.addable_watcher_users.include?(issue.assigned_to)
-          issue.add_watcher(issue.assigned_to)
-      end
-      # Add the current user if they are addable
-      issue.add_watcher(User.current) if issue.addable_watcher_users.include?(User.current.id)
+      issue.add_watcher(issue.assigned_to) if
+        issue.assigned_to &&
+        issue.assigned_to != User.current && # works around addable_watcher_users not being updated by add_watcher
+        !issue.watched_by?(issue.assigned_to)
     end
   end
 
+  def controller_issues_new_before_save(context={})
+    controller_issues_edit_before_save(context)
+  end
 end

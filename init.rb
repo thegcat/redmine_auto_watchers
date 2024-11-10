47f92a52ae708035bedec2b1d30cefcc708677bd
Felix Schäfer <felix@thegcat.net>
2024-11-10 15:18:14 +0100

    Redmine 5 compatibility

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'redmine'
-require 'auto_watchers/hooks/auto_watchers_issue_hook'
+require File.expand_path('lib/auto_watchers/hooks/auto_watchers_issue_hook', __dir__)
 
 Redmine::Plugin.register :redmine_auto_watchers do
   name 'Automatic Watchers'
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_auto_watchers do
   description 'Automatic Watchers is a Redmine project that automatically adds people that touch an issue as watchers."'
   version '1.0.0'
 
-  requires_redmine '3.2'
+  requires_redmine '5.0'
 
   settings default:
     {'enable_watchers' => 'on'},
--- lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
+++ lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
@@ -1,23 +1,29 @@
 # Hook in to the Issues controller and update the watchers before it is
 # saved.
-class AutoWatchersIssuesControllerHook < Redmine::Hook::ViewListener
-  def controller_issues_edit_before_save(context={})
-    configuration = Setting.plugin_redmine_auto_watchers
-    if configuration['enable_watchers']
-      issue = context[:issue]
-      # Add the current user if they are addable
-      issue.add_watcher(User.current) unless issue.watched_by?(User.current)
+module AutoWatchers
+  module Hooks
 
-      # Add the person the ticket is assigned to if it is assigned to
-      # someone and they are addable.
-      issue.add_watcher(issue.assigned_to) if
-        issue.assigned_to &&
-        issue.assigned_to != User.current && # works around addable_watcher_users not being updated by add_watcher
-        !issue.watched_by?(issue.assigned_to)
+    class AutoWatchersIssuerHook < Redmine::Hook::ViewListener
+      def controller_issues_edit_before_save(context={})
+        configuration = Setting.plugin_redmine_auto_watchers
+        if configuration['enable_watchers']
+          issue = context[:issue]
+          # Add the current user if they are addable
+          issue.add_watcher(User.current) unless issue.watched_by?(User.current)
+
+          # Add the person the ticket is assigned to if it is assigned to
+          # someone and they are addable.
+          issue.add_watcher(issue.assigned_to) if
+          issue.assigned_to &&
+            issue.assigned_to != User.current && # works around addable_watcher_users not being updated by add_watcher
+            !issue.watched_by?(issue.assigned_to)
+        end
+      end
+
+      def controller_issues_new_before_save(context={})
+        controller_issues_edit_before_save(context)
+      end
     end
-  end
 
-  def controller_issues_new_before_save(context={})
-    controller_issues_edit_before_save(context)
   end
 end
